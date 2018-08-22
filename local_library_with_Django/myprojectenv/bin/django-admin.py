b7f7855eb510461ab83cd38825a8ff9580481552
AriesShilz <ariesshilz@gmail.com>
2018-08-22 19:00:10 +0530

    initial commit

---
+++ local_library_with_Django/myprojectenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/hp/Desktop/library_django/django-locallibrary-tutorial-master/myprojectenv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
